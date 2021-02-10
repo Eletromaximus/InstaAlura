1b954bf5d6e559f25c45cd523d27b453198e783b
Max Milliano de Souza Machado <maxmillianox@protonmail.com>
2021-02-10 15:05:49 -0300

    feat(headers style): styled components introduction. Add Conventional Commits

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,53 @@
-import styled from 'styled-components'
+import {MenuWrapper} from '../src/components/Menu/styles/index'
+import Logo from '../src/theme/Logo/Logo'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+function Menu() {
+  const links = [
+    {
+      texto: 'Home',
+      url: '/',
+      id: 0
+    },
+    {
+      texto: 'Perguntas frequentes',
+      url: '/',
+      id: 1
+    },
+    {
+      texto: 'Sobre',
+      url: '/',
+      id: 2
+      },
+  ]
+  
+  return (
+  <MenuWrapper>
+    <MenuWrapper.LeftSide>
+      <Logo />
+    </MenuWrapper.LeftSide>
+    <MenuWrapper.CentralSide>
+      {links.map( link => {
+        return (
+          <li key={link.id}>
+            <a href={link.url}>
+              {link.texto}
+            </a>
+          </li>
+        )
+      })}
+    </MenuWrapper.CentralSide>
+    <MenuWrapper.RightSide>
+      <button>
+        Entrar
+      </button>
+      <button>
+        Cadastrar
+      </button>
+    </MenuWrapper.RightSide>
+  </MenuWrapper>
+  )
+}
 
 export default function Home() {
-  return <Title>Olá mundo</Title>
+  return Menu()
 }
---
+++ src/components/Menu/styles/index.ts
@@ -0,0 +1,57 @@
+import styled from 'styled-components'
+
+
+export const  MenuWrapper: any = styled.nav`
+
+  font-family: 'Rubik', sans-serif;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  margin-top: 18px;
+  padding-left: 28px;
+  padding-right: 28px;
+`;
+
+MenuWrapper.LeftSide = styled.div`
+  padding: 0;
+  margin: 0;  
+  order: 1;
+`;
+
+MenuWrapper.CentralSide = styled.div`
+  padding: 0;
+  margin: 0;
+  order: 3;
+  width: 100%;
+  list-style: none;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 17px;
+  border-top: 1px solid #88989E;
+  border-bottom: 1px solid #88989E;
+  padding: 12px; 
+
+  a {
+    text-align: center;
+    display: block;
+    text-decoration: none;
+    color: #88989E;
+    transition: 200ms ease-in-out;
+    &:hover,
+    &:focus {
+      font-weight: 500;
+      color: #070C0E;
+    }
+  }
+`;
+
+MenuWrapper.RightSide = styled.div`
+  padding: 0;
+  margin: 0;
+  order: 2;
+  display: flex;
+  flex: 1;
+  justify-content: flex-end;
+`;
