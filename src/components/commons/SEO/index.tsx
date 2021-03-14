5ab77eedaa07b3bb733e85e53524dbec2104e39c
Max Milliano de Souza Machado <maxmillianox@protonmail.com>
2021-03-14 11:37:23 -0300

    feat(modificando seo, alterando imagem): trocando imagem do SEO

--- src/components/commons/SEO/index.tsx
+++ src/components/commons/SEO/index.tsx
@@ -5,7 +5,7 @@ function SEO ({ headTitle }: any) {
   const hasHeadTitle = Boolean(headTitle)
   const pageTitleDefault = 'Instalura - Projeto Base do Alura Bootcamp'
   const description = 'With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!'
-  const image = 'https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png'
+  const image = 'https://www.alura.com.br/assets/img/alura-share.1571848411.png'
   const urlBase = 'https://metatags.io/'
   const title = hasHeadTitle ? (`${headTitle} |  ${pageTitleDefault}`) : pageTitleDefault
 
