c841976ee269e1beafe375b34f7124bfe12a397a
Max Milliano de Souza Machado <maxmillianox@protonmail.com>
2021-05-27 19:09:44 -0300

    fix(tentativa de correçao de teste de integraçao no ci): colocando o token numa ordem superior

--- src/infra/cms/CMSGraphQLClient.ts
+++ src/infra/cms/CMSGraphQLClient.ts
@@ -3,10 +3,11 @@ import { GraphQLClient, gql as GraphQLTag } from 'graphql-request'
 export const gql = GraphQLTag
 
 export function CMSGraphQLClient ({ preview } = { preview: false }) {
+  const TOKEN = process.env.DATO_CMS_TOKEN
   const DatoCMSURL = preview
     ? 'https://graphql.datocms.com/preview'
     : 'https://graphql.datocms.com/'
-  const TOKEN = process.env.DATO_CMS_TOKEN
+
   const client = new GraphQLClient(DatoCMSURL, {
     headers: {
       Authorization: `Bearer ${TOKEN}`
